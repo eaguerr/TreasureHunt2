d8a0813882d6f8f69cd2aa3a7464ca24531a034f
Eduard Guerra <eaguerra@students.tulsatech.edu>
2019-12-03 08:21:31 -0600

    Added locatios services pop up message

--- Treasure Hunt/MapViewController.swift
+++ Treasure Hunt/MapViewController.swift
@@ -7,10 +7,43 @@
 //
 
 import Foundation
+import CoreLocation
 import UIKit
 import MapKit
 import AVFoundation
 
+
 class MapViewController: UIViewController {
+//Var will go below this  message
+
+
+
+
+
+//IBActions and IBOutlets will go below this message.
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    @IBAction func closeMapButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func mapChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 || sender.selectedSegmentIndex == 1 || sender.selectedSegmentIndex == 2 {
+          mapView.isHidden = false
+          if sender.selectedSegmentIndex == 0 {
+            mapView.mapType = .standard
+          } else if sender.selectedSegmentIndex == 1 {
+            mapView.mapType = .satellite
+          } else if sender.selectedSegmentIndex == 2 {
+            mapView.mapType = .hybrid
+            }
+        } else {
+          mapView.isHidden = true
+        }
+    }
+    
+
+
+
     
 }
